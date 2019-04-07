46c4de11a15190df50d133bb907f50aee7061161
infsolution <clsinfsolution@gmail.com>
2019-04-07 09:14:04 -0300

    adicioanando testes simples nas classes professor e disciplina

--- classman/aplication/models.py
+++ classman/aplication/models.py
@@ -4,6 +4,8 @@ from django.db import models
 class Professor(models.Model):
 	usuario = models.OneToOneField(User, related_name='professor', on_delete=models.CASCADE)
 	matricula = models.CharField(max_length=8, unique=True)
+	def __str__(self):
+		return self.usuario.username
 
 class Disciplina(models.Model):
 	professor = models.ManyToManyField(Professor)
