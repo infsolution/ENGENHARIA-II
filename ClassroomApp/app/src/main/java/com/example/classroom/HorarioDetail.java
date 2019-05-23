9f44cdcdb3f6a3545a2308e23582aaf911485b47
infsolution <clsinfsolution@gmail.com>
2019-05-22 23:26:59 -0300

    add app

---
+++ ClassroomApp/app/src/main/java/com/example/classroom/HorarioDetail.java
@@ -0,0 +1,13 @@
+package com.example.classroom;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HorarioDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_horario_detail);
+    }
+}
---
+++ ClassroomApp/app/src/main/java/com/example/classroom/HorarioHome.java
@@ -0,0 +1,120 @@
+package com.example.classroom;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class HorarioHome extends AppCompatActivity {
+    private Button btSeg1, btSeg2,btSeg3,btSe4,btSeg5, btTer1, btTer2,btTer3,btTer4,btTer5, btQua1,btQua2,btQua3,btQua4,btQua5,btQui1,btQui2,btQui3,btQui4,btQui5,btSex1,btSex2,btSex3,btSex4,btSex5;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_horario_home);
+
+    }
+
+
+    public void goHoraSeg1(View v){
+        Intent intent = new Intent(this, HorarioDetail.class);
+        startActivity(intent);
+    }
+    public void goHoraSeg2(View v){
+        Intent intent = new Intent(this, HorarioDetail.class);
+        startActivity(intent);
+    }
+    public void goHoraSeg3(View v){
+        Intent intent = new Intent(this, HorarioDetail.class);
+        startActivity(intent);
+    }
+    public void goHoraSeg4(View v){
+        Intent intent = new Intent(this, HorarioDetail.class);
+        startActivity(intent);
+    }
+    public void goHoraSeg5(View v){
+        Intent intent = new Intent(this, HorarioDetail.class);
+        startActivity(intent);
+    }
+    public void goHoraTer1(View v){
+        Intent intent = new Intent(this, HorarioDetail.class);
+        startActivity(intent);
+    }
+    public void goHoraTer2(View v){
+        Intent intent = new Intent(this, HorarioDetail.class);
+        startActivity(intent);
+    }
+    public void goHoraTer3(View v){
+        Intent intent = new Intent(this, HorarioDetail.class);
+        startActivity(intent);
+    }
+    public void goHoraTer4(View v){
+        Intent intent = new Intent(this, HorarioDetail.class);
+        startActivity(intent);
+    }
+    public void goHoraTer5(View v){
+        Intent intent = new Intent(this, HorarioDetail.class);
+        startActivity(intent);
+    }
+
+    public void goHoraQua1(View v){
+        Intent intent = new Intent(this, HorarioDetail.class);
+        startActivity(intent);
+    }
+    public void goHoraQua2(View v){
+        Intent intent = new Intent(this, HorarioDetail.class);
+        startActivity(intent);
+    }
+    public void goHoraQua3(View v){
+        Intent intent = new Intent(this, HorarioDetail.class);
+        startActivity(intent);
+    }
+    public void goHoraQua4(View v){
+        Intent intent = new Intent(this, HorarioDetail.class);
+        startActivity(intent);
+    }
+    public void goHoraQua5(View v){
+        Intent intent = new Intent(this, HorarioDetail.class);
+        startActivity(intent);
+    }
+    public void goHoraQui1(View v){
+        Intent intent = new Intent(this, HorarioDetail.class);
+        startActivity(intent);
+    }
+    public void goHoraQui2(View v){
+        Intent intent = new Intent(this, HorarioDetail.class);
+        startActivity(intent);
+    }
+    public void goHoraQui3(View v){
+        Intent intent = new Intent(this, HorarioDetail.class);
+        startActivity(intent);
+    }
+    public void goHoraQui4(View v){
+        Intent intent = new Intent(this, HorarioDetail.class);
+        startActivity(intent);
+    }
+    public void goHoraQui5(View v){
+        Intent intent = new Intent(this, HorarioDetail.class);
+        startActivity(intent);
+    }
+    public void goHoraSex1(View v){
+        Intent intent = new Intent(this, HorarioDetail.class);
+        startActivity(intent);
+    }
+    public void goHoraSex2(View v){
+        Intent intent = new Intent(this, HorarioDetail.class);
+        startActivity(intent);
+    }
+    public void goHoraSex3(View v){
+        Intent intent = new Intent(this, HorarioDetail.class);
+        startActivity(intent);
+    }
+    public void goHoraSex4(View v){
+        Intent intent = new Intent(this, HorarioDetail.class);
+        startActivity(intent);
+    }
+    public void goHoraSex5(View v){
+        Intent intent = new Intent(this, HorarioDetail.class);
+        startActivity(intent);
+    }
+}
